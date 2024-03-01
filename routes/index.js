1062d5f496b56939645a8658d63def4439f192d0
anurgazin <dindog24@gmail.com>
2024-03-01 15:00:27 -0500

    Minor changes in index route

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,19 @@
 const express = require("express");
 const router = express.Router();
+const { version, author, documentation } = require("../package.json");
 
 /* GET home page. */
 router.get("/", function (req, res, next) {
-  res.send("Welcome to the Pinterest API, please look at the documentation for more information");
+  res.send(
+    "Welcome to the Pinterest API, please look at the documentation for more information"
+  );
+});
+router.get("/documentation", function (req, res, next) {
+  res.status(200).json({
+    message: "Read Documentation Here: " + documentation,
+    author: author,
+    version: version,
+  });
 });
 
 module.exports = router;
